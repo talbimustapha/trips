cdd72288765fbde8f527b5d9a764b194d4c8df59
TALBI EL MUSTAPHA <113144768+talbimustapha@users.noreply.github.com>
2023-12-07 14:36:07 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,9 @@
+
+
+    let popup = document.getElementById("popup");
+function openpop() {
+   popup.classList.add("open-pop") 
+}
+function closepop() {
+    popup.classList.remove("open-pop") 
+ }
